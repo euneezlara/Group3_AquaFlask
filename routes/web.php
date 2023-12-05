30d23df2982540f802f902550921f145b57ec912
euneezlara <115857403+euneezlara@users.noreply.github.com>
2023-12-05 10:30:14 +0800

    HomePage frontend

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\admin\ProductSubCategoryController;
 use App\Http\Controllers\admin\SubCategoryController;
 use App\Http\Controllers\admin\TempImagesController;
 use App\Http\Controllers\admin\ProductImageController;
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -24,10 +25,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+Route::get('/',[FrontController::class,'index'])->name('front.home');
 
 
 Route::group(['prefix' => 'admin'], function () {
